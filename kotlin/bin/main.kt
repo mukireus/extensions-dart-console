030bc117399ac20121ed8e74681d5a0f550986aa
Muhammet ÖMER <muhammet.omer@outlook.com>
2020-07-11 20:47:37 +0300

    added example kotlin extension

---
+++ kotlin/bin/main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var bridgeLength : Int = 2.km + 682.m()
+    println("Osmangazi Koprusunun uzunlugu $bridgeLength metredir.")
+    var footballStadiumLength : Double = 360.ft()
+    println("Futbol sahasinin uzunlugu $footballStadiumLength metredir.")
+}
---
+++ kotlin/lib/extension.kt
@@ -0,0 +1,5 @@
+fun Int.km(): Int { return (this * 1000) }
+fun Int.m(): Int { return (this * 1) }
+fun Int.cm(): Double { return (this / .100) }
+fun Int.mm(): Double { return (this / .1000) }
+fun Int.ft(): Double { return (this / 3.28084) }
