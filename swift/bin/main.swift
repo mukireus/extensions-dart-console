3b62ddf7a9b994f7e93f06b8cc8e7ff72fa5143d
Muhammet ÖMER <muhammet.omer@outlook.com>
2020-07-12 02:42:56 +0300

    added example swift extension

---
+++ swift/bin/main.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+let bridgeLength = 2.km + 682.m
+print("Osmangazi Köprüsünün uzunluğu \(bridgeLength) metredir.")
+let footballStadiumLength = 360.ft
+print("Futbol sahasının uzunluğu \(footballStadiumLength) metredir.")
